379eefde4b263cf29a7cfaf3eefda3d11f524929
yangchenyun@gmail.com <yangchenyun@gmail.com>
2012-06-03 16:50:54 +0800

    spec to test the course processor

---
+++ spec/sm_to_anki_spec.rb
@@ -0,0 +1,52 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'sm_to_anki'
+
+describe SmToAnki::CourseProcessor do
+
+    before do
+      @working_dir = "#{File.dirname(__FILE__)}/../fixtures"
+      @course_processor = SmToAnki::CourseProcessor.new("#@working_dir")
+      @course_processor.fetch_course_info()
+      puts @course_processor.course_info
+    end
+
+    it "process_dir should be set to current working directory" do
+      @course_processor.process_dir.must_equal @working_dir
+    end
+
+    it "the course.xml should be opened and parsed by nokogiri" do
+      @course_processor.course_doc.must_be_instance_of Nokogiri::XML::Document
+    end
+    
+    # fetch course.xml data into course_info instance variable
+    it "should store the fetched data in course_info hash" do
+      @course_processor.course_info.must_be_instance_of Hash
+    end
+
+    it "should fetch the course title right" do
+      @course_processor.course_info.has_key?("title").must_equal true
+      @course_processor.course_info['title'] == "Fake Course"
+    end
+
+    it "element[type=pres] will store the [:name] as hash keys " do
+      @course_processor.course_info.has_key?('Category1').must_equal true
+      @course_processor.course_info.has_key?('Category2').must_equal true
+      @course_processor.course_info['Category2'].has_key?('sub-category1').must_equal true
+    end
+
+    it "element[type=pres] hierarchy will be preserved" do
+      @course_processor.course_info.has_key?('sub-category1').must_equal false
+      @course_processor.course_info['Category2'].has_key?('sub-category1').must_equal true
+      @course_processor.course_info['Category2']['sub-category2'].has_key?("category-level-3").must_equal true
+    end
+    
+    it "exercises will be stored in an array and grouped by its parent" do
+      @course_processor.course_info['Category1'].must_be_instance_of Array
+      @course_processor.course_info['Category1'].length.must_equal 2
+    end
+
+    it "should fetch the id from element[type=exercise] and keep 5 digitals" do
+      @course_processor.course_info['Category1'].must_equal ['00002', '00003']
+    end
+end
